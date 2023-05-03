516229bc8442d7ad9c84b0800a20106de2346b2e
Altoiids <somyachawla0250@gmail.com>
2023-05-04 04:50:47 +0530

    review krdo

---
+++ home.js
@@ -0,0 +1,10 @@
+
+
+
+    document.getElementById("rulesbtn").onclick = function () {
+        location.href = "rules.html";
+    };
+
+document.getElementById("startbtn").onclick = function () {
+        location.href = "index.html";
+    };
