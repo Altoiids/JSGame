e1710fdc8db83d4f1ca6dc5dace8b8b4146e1348
Altoiids <somyachawla0250@gmail.com>
2023-05-04 17:13:56 +0530

    final review

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,7 @@
-
-
-
-    document.getElementById("rulesbtn").onclick = function () {
-        location.href = "rules.html";
-    };
+document.getElementById("rulesbtn").onclick = function () {
+  location.href = "rules.html";
+};
 
 document.getElementById("startbtn").onclick = function () {
-        location.href = "../../home.html";
-    };
+  location.href = "../../home.html";
+};
