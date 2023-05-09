4fc3cb5473adc12d983cf2bf11710a49aed73140
Altoiids <somyachawla0250@gmail.com>
2023-05-09 23:19:53 +0530

    code repetition removed

--- public/js/script.js
+++ public/js/script.js
@@ -13,8 +13,12 @@ box2 = document.querySelector(".darkhouseimg");
 score = document.querySelector(".score");
 santa = document.querySelector(".santaimg");
 divabove = document.querySelector(".divabove");
-anisanta = parseFloat(window.getComputedStyle(santa, null).getPropertyValue('animation-duration'));
-anigift = parseFloat(window.getComputedStyle(divabove, null).getPropertyValue('animation-duration'));
+anisanta = parseFloat(
+  window.getComputedStyle(santa, null).getPropertyValue("animation-duration")
+);
+anigift = parseFloat(
+  window.getComputedStyle(divabove, null).getPropertyValue("animation-duration")
+);
 var rect = gift.getBoundingClientRect();
 var rect1 = box1.getBoundingClientRect();
 var rect2 = box2.getBoundingClientRect();
@@ -40,29 +44,27 @@ document.addEventListener("keydown", (event) => {
     }, 400);
   }
 
-  
-
-  if (event.code == "Space" &&
-  gift.offsetLeft <= box2.offsetLeft &&
-  gift.offsetLeft <= box2.offsetLeft + rect2.width &&
-  rect.top <= rect2.top &&
-  rect.top <= rect2.top + rect2.height &&
-  gift.offsetLeft >= rect1.left &&
-  gift.offsetLeft <= rect1.left + box1.clientWidth / 2 &&
-  rect.top <= rect1.top &&
-  rect.top <= rect1.top + box1.clientHeight
+  if (
+    event.code == "Space" &&
+    gift.offsetLeft <= box2.offsetLeft &&
+    gift.offsetLeft <= box2.offsetLeft + rect2.width &&
+    rect.top <= rect2.top &&
+    rect.top <= rect2.top + rect2.height &&
+    gift.offsetLeft >= rect1.left &&
+    gift.offsetLeft <= rect1.left + box1.clientWidth / 2 &&
+    rect.top <= rect1.top &&
+    rect.top <= rect1.top + box1.clientHeight
   ) {
     Life -= 1;
-    
+
     Lives.innerHTML = "Lives : " + Life;
     if (Life == 0) {
-     gameOver(); 
+      gameOver();
     }
   }
 });
 
 document.addEventListener("keydown", (event) => {
-  
   if (event.code == "KeyM") {
     audio.pause();
   }
@@ -76,9 +78,6 @@ setInterval(() => {
   score = document.querySelector(".score");
   santa = document.querySelector(".santaimg");
   divabove = document.querySelector(".divabove");
-  anisanta = parseFloat(window.getComputedStyle(santa, null).getPropertyValue('animation-duration'));
-anigift = parseFloat(window.getComputedStyle(divabove, null).getPropertyValue('animation-duration'));
-
   var rect = gift.getBoundingClientRect();
   var rect1 = box1.getBoundingClientRect();
   var rect2 = box2.getBoundingClientRect();
@@ -90,8 +89,6 @@ anigift = parseFloat(window.getComputedStyle(divabove, null).getPropertyValue('a
     rect.top <= rect1.top + box1.clientHeight
   ) {
     gameOver();
-
-    
   }
 
   if (
@@ -110,68 +107,52 @@ anigift = parseFloat(window.getComputedStyle(divabove, null).getPropertyValue('a
     }, 1000);
   }
 
-
-  if (scoreval >= 1 && scoreval < 5 && santa.style.animationDuration < "4s" ) {
-
-    setTimeout(() => {
-      
-      
-      newanisanta = anisanta - 0.0000001;
-      newanigift = anigift - 0.0000001;
-      santa.style.animationDuration = newanisanta + 's';
-      divabove.style.animationDuration = newanigift + 's';
-     
-  }, 5);
-  } 
-  
-  else if (scoreval >= 5 && scoreval < 10 &&  santa.style.animationDuration < "3.5s") {
- 
-    setTimeout(() => {
-      newanisanta = anisanta - 0.0000001;
-      newanigift = anigift - 0.0000001;
-      santa.style.animationDuration = newanisanta + 's';
-      divabove.style.animationDuration = newanigift + 's';
-      
-  }, 5);
-
+  if (scoreval >= 1 && scoreval < 5 && santa.style.animationDuration < "4s") {
     
-  } else if (scoreval >= 10 && scoreval < 15 && santa.style.animationDuration < "3s" ) {
+      
+    setTimeout(() => {
    
+      changeani();
+    }, 5);
+   
+  } else if (
+    scoreval >= 5 &&
+    scoreval < 10 &&
+    santa.style.animationDuration < "3.5s"
+  ) {
     setTimeout(() => {
-      
-      newanisanta = anisanta - 0.0000001;
-      newanigift = anigift - 0.0000001;
-      santa.style.animationDuration = newanisanta + 's';
-      divabove.style.animationDuration = newanigift + 's';
      
-  }, 5);
-
-
-  } else if (scoreval >= 15 && scoreval < 20 && santa.style.animationDuration < "2.5s" ) {
-    
+      changeani();
+    }, 5);
+  } else if (
+    scoreval >= 10 &&
+    scoreval < 15 &&
+    santa.style.animationDuration < "3s"
+  ) {
     setTimeout(() => {
-      
-      newanisanta = anisanta - 0.0000001;
-      newanigift = anigift - 0.0000001;
-      santa.style.animationDuration = newanisanta + 's';
-      divabove.style.animationDuration = newanigift + 's';
-      
-  }, 5);
-
-  } else if (scoreval >= 20 && scoreval < 25 && santa.style.animationDuration < "2s" ) {
-
+   
+      changeani();
+    }, 5);
+  } else if (
+    scoreval >= 15 &&
+    scoreval < 20 &&
+    santa.style.animationDuration < "2.5s"
+  ) {
     setTimeout(() => {
-    
-      newanisanta = anisanta - 0.0000001;
-      newanigift = anigift - 0.0000001;
-      santa.style.animationDuration = newanisanta + 's';
-      divabove.style.animationDuration = newanigift + 's';
+   
+      changeani();
+    }, 5);
+  } else if (
+    scoreval >= 20 &&
+    scoreval < 25 &&
+    santa.style.animationDuration < "2s"
+  ) {
+    setTimeout(() => {
+   
       
-  }, 5);
-
-
-  } 
-  
+      changeani();
+    }, 5);
+  }
 
   checkHighScore();
 }, 10);
@@ -180,8 +161,6 @@ function updateScore(scoreval) {
   santa = document.querySelector(".santaimg");
   var rect3 = santa.getBoundingClientRect();
   score.innerHTML = "Score: " + scoreval;
-
-  
 }
 function updatehighScore(highScore) {
   highscore.innerHTML = "Highest Score: " + highScore;
@@ -193,24 +172,39 @@ function checkHighScore() {
   }
   document.getElementById("highScore").innerHTML = "Highscore: " + highScore;
 }
- 
-function gameOver(){
-  gameover.innerHTML =
-        "Game Over - You Will be Shortly Redirected to Home Page";
-      box1.classList.remove("lighthouseani");
-      box2.classList.remove("darkhouseani");
-      divabove.classList.remove("santaani");
-      santa.classList.remove("santaani");
-      santa.style.display = "none";
-      box1.style.display = "none";
-      box2.style.display = "none";
-    audioend.play();
-    setTimeout(() => {
-      audioend.pause();
-      audio.pause();
-    }, 1000);
 
-    setTimeout(() => {
-      location.href = "index.html";
-    }, 4000);
+function gameOver() {
+  gameover.innerHTML =
+    "Game Over - You Will be Shortly Redirected to Home Page";
+  box1.classList.remove("lighthouseani");
+  box2.classList.remove("darkhouseani");
+  divabove.classList.remove("santaani");
+  santa.classList.remove("santaani");
+  santa.style.display = "none";
+  box1.style.display = "none";
+  box2.style.display = "none";
+  audioend.play();
+  setTimeout(() => {
+    audioend.pause();
+    audio.pause();
+  }, 1000);
+
+  setTimeout(() => {
+    location.href = "index.html";
+  }, 4000);
+}
+
+function changeani() {
+ 
+  console.log("function called");
+      anisanta = parseFloat(window.getComputedStyle(santa, null).getPropertyValue('animation-duration'));
+      anigift = parseFloat(window.getComputedStyle(divabove, null).getPropertyValue('animation-duration'));
+      newanisanta = anisanta - 0.0000001;
+      newanigift = anigift - 0.0000001;
+      santa.style.animationDuration = newanisanta + 's';
+      divabove.style.animationDuration = newanigift + 's';
+      console.log('New animation duration: ', newanisanta);
+      console.log('New animation duration: ', newanigift);
+  
+
 }
